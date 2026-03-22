639548db6c211594ceb93bfdc1828b3c511cf25a
Aadish Garg <aadishgarg15@gmail.com>
2026-03-22 16:10:19 +0530

    backend changed

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -174,7 +174,7 @@ if (predictForm) {
     resultBar.style.width = '0';
 
     try {
-      const res = await fetch('http://127.0.0.1:5000/predict', {
+      const res = await fetch('https://episight-ai.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
